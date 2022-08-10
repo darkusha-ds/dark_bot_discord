7b92f4ca8af561c54b4c905d3feab283c0517162
darkusha-ds <mts04@inbox.ru>
2022-08-11 00:37:08 +0500

    update

--- cogs/afk.py
+++ cogs/afk.py
@@ -18,22 +18,14 @@ class afk(commands.Cog):
     @commands.command(pass_context=True)
     @commands.has_any_role(*roles)
     @commands.cooldown(1, 1200, commands.BucketType.user)
-    async def afk(self, ctx, *, reason=None):
+    async def afk(self, ctx):
         cam = ctx.author.mention
         can = ctx.author.display_name
         cci = ctx.channel.id
         if cci in channels:
-            if reason is None:
-                await ctx.send(
-                    embed=discord.Embed(
-                        color=0xff9900,
-                        title=f'{can}#{ctx.author.discriminator}, **укажите причину**',
-                        description=f'Пример: {pref}afk **причина**'
-                ), delete_after=time_5s)
-            else:
-                nick = f"{afk_pref} {can}"
-                await ctx.author.edit(nick=nick)
-                await ctx.send(f'{cam} ушел в афк по причине ***{reason}***')
+            nick = f"{afk_pref} {can}"
+            await ctx.author.edit(nick=nick)
+            await ctx.send(f'{cam} ушел в АФК')
         else:
             await ctx.send(error_message, delete_after=time_10s)
     
@@ -42,13 +34,11 @@ class afk(commands.Cog):
     @commands.has_any_role(*roles)
     @commands.cooldown(rate=1, per=60, type=commands.BucketType.user)
     async def un_afk(self, ctx):
-        # mam = ctx.author.mention
-        # mum = member.mention
         cci = ctx.channel.id
         current_nick = ctx.author.nick
+        old_nick = current_nick.replace(afk_pref,"")
         if cci in channels:
             if afk_pref in current_nick:
-                old_nick = current_nick.replace(afk_pref,"")
                 await ctx.author.edit(nick=old_nick)
                 await ctx.send(f'{ctx.author.mention} вышел из АФК')
             else:
