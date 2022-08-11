3d8cb2f3349191629823d42344e06b63331e0761
darkusha-ds <mts04@inbox.ru>
2022-08-12 00:45:39 +0500

    update afk

--- cogs/afk.py
+++ cogs/afk.py
@@ -17,34 +17,20 @@ class afk(commands.Cog):
 
     @commands.command(name='afk', aliases=aliaces_afk, pass_context=True)
     @commands.has_any_role(*roles)
-    @commands.cooldown(1, 1200, commands.BucketType.user)
+    @commands.cooldown(1, 600, commands.BucketType.user)
     async def afk(self, ctx):
-        cam = ctx.author.mention
-        can = ctx.author.display_name
-        cci = ctx.channel.id
-        if cci in channels:
-            nick = f"{afk_pref} {can}"
-            await ctx.author.edit(nick=nick)
-            await ctx.send(f'{cam} ушел в АФК')
-        else:
-            await ctx.send(error_message, delete_after=time_10s)
-    
-
-    @commands.command(name='un_afk', aliases=aliaces_unafk, pass_context=True)
-    @commands.has_any_role(*roles)
-    @commands.cooldown(rate=1, per=60, type=commands.BucketType.user)
-    async def un_afk(self, ctx):
-        cci = ctx.channel.id
         current_nick = ctx.author.nick
         old_nick = current_nick.replace(afk_pref,"")
+        cci = ctx.channel.id
         if cci in channels:
             if afk_pref in current_nick:
                 await ctx.author.edit(nick=old_nick)
                 await ctx.send(f'{ctx.author.mention} вышел из АФК')
             else:
-                await ctx.send('Вы не были в режиме АФК')
+                nick = f"{afk_pref} {current_nick}"
+                await ctx.author.edit(nick=nick)
+                await ctx.send(f'{current_nick} ушел в АФК')
         else:
-            await ctx.channel.purge(limit=1)
             await ctx.send(error_message, delete_after=time_10s)
 
 def setup(bot):
