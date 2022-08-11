966db58332c49cf861be3d709948c8cb0311fe64
darkusha-ds <mts04@inbox.ru>
2022-08-11 20:30:28 +0500

    update

--- cogs/roles.py
+++ cogs/roles.py
@@ -16,18 +16,8 @@ class roles(commands.Cog):
     @commands.command(name='rols', aliases=aliaces_roles)
     @commands.has_permissions(administrator=True)
     async def get_roles_id(self, ctx):
-        cci = ctx.channel.id
-        if cci == channels[0]:
-            guild = bot.get_guild(servers_id[0])
-            await ctx.send(guild.roles)
-
-        elif cci == channels[2]:
-            guild = bot.get_guild(servers_id[1])
-            await ctx.send(guild.roles)
-
-        elif cci == channels[4]:
-            guild = bot.get_guild(servers_id[2])
-            await ctx.send(guild.roles)
+        guild = bot.get_guild(870237741739302912)
+        await ctx.send(guild.roles)
 
 def setup(bot):
     bot.add_cog(roles(bot))
